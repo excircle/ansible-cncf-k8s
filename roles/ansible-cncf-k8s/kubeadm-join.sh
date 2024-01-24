3e48d88aab79d08047b52db301a38a4d2910e3fc
Alexander Kalaj <unixdisciple.us@gmail.com>
2024-01-24 15:55:09 -0800

    Successful refactor of local join script - Master bootstrap complete

--- roles/ansible-cncf-k8s/kubeadm-join.sh
+++ roles/ansible-cncf-k8s/kubeadm-join.sh
@@ -1,1 +1,2 @@
-["skipped, since /etc/kubernetes/admin.conf exists"]
+kubeadm join k8scp:6443 --token 0k0ttj.g5x30qa9bbgci2w8 \
+	--discovery-token-ca-cert-hash sha256:14029b20fc308c9d5f178bc6020d6d46ce73cb519dfc4c34c9cbf773d358da68 
