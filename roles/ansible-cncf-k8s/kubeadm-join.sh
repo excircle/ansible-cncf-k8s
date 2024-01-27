013b65cfb776f9cfa3b20f85646291db5dcce969
Alexander Kalaj <unixdisciple.us@gmail.com>
2024-01-27 08:19:48 -0800

    COMPLETE - Successful boot strap of multiple nodes from scratch

--- roles/ansible-cncf-k8s/kubeadm-join.sh
+++ roles/ansible-cncf-k8s/kubeadm-join.sh
@@ -1,2 +1,2 @@
-kubeadm join k8scp:6443 --token 0k0ttj.g5x30qa9bbgci2w8 \
-	--discovery-token-ca-cert-hash sha256:14029b20fc308c9d5f178bc6020d6d46ce73cb519dfc4c34c9cbf773d358da68 
+kubeadm join k8scp:6443 --token m0gbew.uztix3xojthqwf56 \
+	--discovery-token-ca-cert-hash sha256:562ce58f2129a717adf15ead7ab416fb9ae943b267df3c0de02ec0621e1da79b 
